172cc1d2f69f76986fec3d3c13b2e04dd24fadc3
Michal Zlatkovský <michal@zlatkovsky.cz>
2024-12-16 14:45:36 +0100

    data upd

--- osmium-scripts/prepare.sh
+++ osmium-scripts/prepare.sh
@@ -6,3 +6,6 @@ sh osmium-scripts/merge.sh cz at
 sh osmium-scripts/convertToGeojson.sh cz at at-cz
 tsx pruneData.ts cz at at-cz
 rm data/*.tmp.*
+npm run check at-cz
+npm run check at
+npm run check cz
