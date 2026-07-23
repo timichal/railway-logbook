e85c67de7000c805f6041da45ba96f9156ebf40e
Michal Zlatkovský <michal@zlatkovsky.cz>
2026-07-23 09:44:15 +0200

    rm console.log

--- src/lib/getUntimezonedDateStr.ts
+++ src/lib/getUntimezonedDateStr.ts
@@ -1,5 +1,4 @@
 export const getUntimezonedDateStr = (dateInput: string | Date): string => {
-  console.log(dateInput, new Date(dateInput).getTimezoneOffset());
   return typeof dateInput === "string"
     ? dateInput.split("T")[0]
     : new Date(dateInput.getTime() - dateInput.getTimezoneOffset() * 60000)
